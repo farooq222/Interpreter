fcff9cd32f0c55dc244f0363f1ea0fb14b9f3364
Muhammad Farooq Arshad <farooq.arshad@hotmail.com>
2015-02-25 21:58:11 +0500

    Create Unit_test1.java

---
+++ Unit_test1.java
@@ -0,0 +1,11 @@
+
+public class Unit_test1 {
+
+	public static void main(String[] args) {
+		Lab2 a = new Lab2();
+		a.read_file("C:\\Users\\Farooq Arshad\\workspace\\Interpreter\\src\\sample1.txt");
+		a.checkline();
+
+	}
+
+}
